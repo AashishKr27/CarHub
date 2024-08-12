9a206037860ebf6482b5135553214dc92673f86a
AashishKr27 <ashish.21gcebds063@galgotiacollege.edu>
2024-08-13 00:41:07 +0530

    commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,8 +31,8 @@ const Hero = () => {
             fill
             className="object-contain"
           />
-          <div className="hero__image-overlay" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
