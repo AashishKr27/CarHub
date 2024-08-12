01337ca88becb7ed48c17ce125010bf62bc7f40a
AashishKr27 <ashish.21gcebds063@galgotiacollege.edu>
2024-08-13 01:06:16 +0530

    commit

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,10 @@
+import { CustomButtonProps } from '@/types'
+import React from 'react'
+
+const CustomFilter = ({ title }: CustomFilterProps) => {
+  return (
+    <div>CustomFilter</div>
+  )
+}
+
+export default CustomFilter
--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Hero from "./Hero";
 import CustomButtom from "./CustomButtom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
     Hero,
     CustomButtom,
     Navbar,
     Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
 }
